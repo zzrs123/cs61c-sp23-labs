9b24687225b3c7440d29a4c245783fcb14e021e1
zzrs123 <2296704779@qq.com>
2023-03-09 15:19:34 +0800

    finish lab01

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -1,6 +1,25 @@
 #include <stddef.h>
 #include "ex10_ll_cycle.h"
+// 0309-15点虽然通过了测试但是写的比较丑
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node * fast_ptr=head;
+    node * slow_ptr=head;
+    
+    // int tag=0;
+    // if(NULL==head)return tag;
+    while(fast_ptr!=NULL && fast_ptr->next!=NULL){
+        fast_ptr = fast_ptr->next->next;
+        slow_ptr = slow_ptr->next;
+        if(fast_ptr==slow_ptr)
+            return 1;
+        // tag=0;
+    }
+    if(fast_ptr == NULL || fast_ptr->next ==NULL) {
+            // tag = 0;
+            return 0;
+    }
+    return 0;
+
 }
--- lab01/ex4_ptr_heap_stack.c
+++ lab01/ex4_ptr_heap_stack.c
@@ -3,7 +3,10 @@
 
 int* int_on_stack() {
   // Allocates memory on the stack for an integer
-  int x = 5;
+  // int x = 5; 局部变量在函数返回后就会被释放，无法拿到其地址
+  
+  // 改为静态，放在静态存储区（数据段）
+  static int x = 5;
 
   // Returns a pointer that points to the number 5
   return &x;
@@ -11,17 +14,21 @@ int* int_on_stack() {
 
 int* int_on_heap() {
   // TODO: allocate memory on the heap for an integer
-  ______ ptr_to_5 = ______;
+  int * ptr_to_5 = (int*)malloc(sizeof(int*));
+
 
   // TODO: store the number 5 in memory you just allocated
-  ______ = 5;
+  *ptr_to_5 = 5;
 
   // Returns a pointer that points to the number 5
   return ptr_to_5;
 }
 
 int main() {
+  // 堆栈的概念有点忘记了
+  // ptr_to_stack-> address of x
   int* ptr_to_stack = int_on_stack();
+  // 
   int* ptr_to_heap = int_on_heap();
 
   printf("ptr_to_stack is the address %p\n", ptr_to_stack);
